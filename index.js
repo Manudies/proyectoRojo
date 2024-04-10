249fac0c4b4ea7d8fa8d66fa268a209b54d917c0
Gandino1984 <andinogerman@gmail.com>
2024-04-10 15:27:49 +0200

    feat: html_js_css_ First day

---
+++ index.js
@@ -0,0 +1,10 @@
+const savePreferencesButton = document.getElementById("savePreferencesButton");
+
+const preferencesContainer_index = document.getElementById("preferencesContainer_index");
+
+
+savePreferencesButton.addEventListener("click",  ()=>{
+
+  preferencesContainer_index.style.display="none";
+
+});
