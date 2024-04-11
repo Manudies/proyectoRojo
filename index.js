427aa13938318ebc39785daac70435ce414a9a97
Gandino1984 <andinogerman@gmail.com>
2024-04-11 14:08:16 +0200

    feat: natxo y german crearon funcion filtrar por tipo sin duplicidades

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ const preference4 = document.getElementById("preference4");
 const preference5 = document.getElementById("preference5");
 const preference6 = document.getElementById("preference6");
 
+const divEvents = document.getElementById("divEvents");
+
 
 preference1.addEventListener("click",  ()=>{
 
@@ -45,3 +47,46 @@ preference6.addEventListener("click",  ()=>{
   namePreferenceTxt.innerText="GRUPOS";
 
 });
+
+// *********
+
+
+async function getData(){
+  
+    const url = new URL("https://api.euskadi.eus/culture/events/v1.0/events")
+    url.searchParams.append("_elements",100);
+
+    const response = await fetch(url.toString());
+    
+    const data =  await response.json();
+
+    addEvents(data.items);
+
+}
+
+function addEvents(events){
+    
+    let typeArray = [];
+    // let name;
+
+    for(let i=0; i < events.length; i++){
+    
+        typeArray[i] =  events[i].typeEs;
+        
+    }
+
+    const ns = new Set(typeArray);
+    const uniqueTypeArray = [...ns];
+    console.log(uniqueTypeArray);
+
+    for(let j=0; j < uniqueTypeArray.length; j++){
+    
+      const name = document.createElement("p");
+      name.innerText = uniqueTypeArray[j]; 
+      divEvents.appendChild(name);
+      
+    }
+    
+}
+
+getData();
