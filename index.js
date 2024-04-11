baf1f5e892d75c89dc7d89dd4b0874664561122d
Gandino1984 <andinogerman@gmail.com>
2024-04-11 11:12:04 +0200

    feat:  genero descripción de botones

--- index.js
+++ index.js
@@ -1,10 +1,47 @@
-const savePreferencesButton = document.getElementById("savePreferencesButton");
-
-const preferencesContainer_index = document.getElementById("preferencesContainer_index");
 
 
-savePreferencesButton.addEventListener("click",  ()=>{
+const namePreferenceTxt = document.getElementById("namePreferenceTxt");
 
-  preferencesContainer_index.style.display="none";
+const preference1 = document.getElementById("preference1");
+const preference2 = document.getElementById("preference2");
+const preference3 = document.getElementById("preference3");
+const preference4 = document.getElementById("preference4");
+const preference5 = document.getElementById("preference5");
+const preference6 = document.getElementById("preference6");
+
+
+preference1.addEventListener("click",  ()=>{
+
+  namePreferenceTxt.innerText="FIESTA";
+
+});
+
+preference2.addEventListener("click",  ()=>{
+
+  namePreferenceTxt.innerText="INTERIOR";
+
+});
+
+preference3.addEventListener("click",  ()=>{
+
+  namePreferenceTxt.innerText="CULTURAL";
+
+});
+
+preference4.addEventListener("click",  ()=>{
+
+  namePreferenceTxt.innerText="GASTRONOMÍA";
+
+});
+
+preference5.addEventListener("click",  ()=>{
+
+  namePreferenceTxt.innerText="AIRE LIBRE";
+
+});
+
+preference6.addEventListener("click",  ()=>{
+
+  namePreferenceTxt.innerText="GRUPOS";
 
 });
