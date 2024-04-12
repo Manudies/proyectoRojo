b7a0f5925545f1538cfab71a75bbe81a7c9bfbac
Gandino1984 <andinogerman@gmail.com>
2024-04-12 21:10:53 +0200

    feat: modificación diseño del modal incial

--- index.js
+++ index.js
@@ -1,76 +1,118 @@
 
+const containerNamePage_index = document.getElementById("containerNamePage_index");
 
 const namePreferenceTxt = document.getElementById("namePreferenceTxt");
 
-const preference1 = document.getElementById("preference1");
-const preference2 = document.getElementById("preference2");
-const preference3 = document.getElementById("preference3");
-const preference4 = document.getElementById("preference4");
-const preference5 = document.getElementById("preference5");
-const preference6 = document.getElementById("preference6");
+// const preference1 = document.getElementById("preference1");
+// const preference2 = document.getElementById("preference2");
+// const preference3 = document.getElementById("preference3");
+// const preference4 = document.getElementById("preference4");
+// const preference5 = document.getElementById("preference5");
+// const preference6 = document.getElementById("preference6");
+// const preference7 = document.getElementById("preference7");
+// const preference8 = document.getElementById("preference8");
+
 
 const divEvents = document.getElementById("divEvents");
 
 const preferencesContainer_index = document.getElementById("preferencesContainer_index");
 
+const showPreferencesButton = document.getElementById("showPreferencesButton");
+
+
 //  ANIMACIÓN CONTENEDOR DE PREFERENCES
 
   setTimeout(() => {
     preferencesContainer_index.style.display="flex";
+    containerNamePage_index.style.display="flex";
+    showPreferencesButton.style.opacity="1";
+    
   }, "200");
 
   setTimeout(() => {
     preferencesContainer_index.style.transform="translateY(0%)";
+    showPreferencesButton.style.display="flex";
   }, "500");
 
   setTimeout(() => {
     preferencesContainer_index.style.opacity="1";
+    containerNamePage_index.style.opacity="1";
+    
   }, "400");
-
-
-
-
-
-
 
 
 // *********
 
 preference1.addEventListener("click",  ()=>{
 
-  namePreferenceTxt.innerText="¡MARCHA!";
-
+  namePreferenceTxt.innerText="¡FIESTA!";
+  
 });
 
 preference2.addEventListener("click",  ()=>{
 
-  namePreferenceTxt.innerText="DE INTERIOR";
+  namePreferenceTxt.innerText="ARTE";
 
 });
 
 preference3.addEventListener("click",  ()=>{
 
-  namePreferenceTxt.innerText="CULTURETA";
+  namePreferenceTxt.innerText="MÚSICA";
 
 });
 
 preference4.addEventListener("click",  ()=>{
 
-  namePreferenceTxt.innerText="GASTROPINTXO";
+  namePreferenceTxt.innerText="GASTRONOMÍA";
 
 });
 
 preference5.addEventListener("click",  ()=>{
 
-  namePreferenceTxt.innerText="AL SOLETE";
+  namePreferenceTxt.innerText="AL AIRE LIBRE";
 
 });
 
 preference6.addEventListener("click",  ()=>{
 
-  namePreferenceTxt.innerText="ARREJUNTAUS";
+  namePreferenceTxt.innerText="DEPORTES";
 
 });
+
+preference7.addEventListener("click",  ()=>{
+
+  namePreferenceTxt.innerText="ALTERNATIVO";
+
+});
+
+preference8.addEventListener("click",  ()=>{
+
+  namePreferenceTxt.innerText="RANDOM";
+  
+});
+
+// *********
+
+showPreferencesButton.addEventListener("click", ()=>{
+
+  setTimeout(() => {
+    preferencesContainer_index.style.display="none";
+    showPreferencesButton.style.opacity="0";
+  }, "400");
+
+  setTimeout(() => {
+    preferencesContainer_index.style.transform="translateY(30%)";
+  }, "200");
+
+  setTimeout(() => {
+    preferencesContainer_index.style.opacity="0";
+    containerNamePage_index.style.opacity="0";
+    containerNamePage_index.style.display="none";
+    showPreferencesButton.style.display="none";
+  }, "200");
+
+});
+
 
 // *********
 
