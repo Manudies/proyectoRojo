9931790e2f3aab334e7b8481ad0b97efab05a10e
Gandino1984 <andinogerman@gmail.com>
2024-04-15 16:44:48 +0200

    cambios de diseño

--- index.js
+++ index.js
@@ -1,188 +1,127 @@
+const preferencesMenu_index = document.getElementById("preferencesMenu_index");
 
-const containerNamePage_index = document.getElementById("containerNamePage_index");
+const preferencesToggle = document.getElementById("preferencesToggle");
 
-const namePreferenceTxt = document.getElementById("namePreferenceTxt");
+const containerPreferenceBtnDescription_index = document.getElementById("containerPreferenceBtnDescription_index");
 
-// const preference1 = document.getElementById("preference1");
-// const preference2 = document.getElementById("preference2");
-// const preference3 = document.getElementById("preference3");
-// const preference4 = document.getElementById("preference4");
-// const preference5 = document.getElementById("preference5");
-// const preference6 = document.getElementById("preference6");
-// const preference7 = document.getElementById("preference7");
-// const preference8 = document.getElementById("preference8");
+const preferenceBtnDescriptionTxt = document.getElementById("preferenceBtnDescriptionTxt");
 
+const vamosBtn_index = document.getElementById("vamosBtn_index");
 
-const divEvents = document.getElementById("divEvents");
+// ***
 
-const preferencesContainer_index = document.getElementById("preferencesContainer_index");
+preferencesToggle.addEventListener("mouseover", ()=>{
+  preferencesToggle.style.transform = "rotate(1turn)";
+});
 
-const showPreferencesButton = document.getElementById("showPreferencesButton");
+preferencesToggle.addEventListener("mouseleave", ()=>{
+  preferencesToggle.style.transform = "rotate(-1turn)";
+});
 
+// ***
 
-//  ANIMACIÓN CONTENEDOR DE PREFERENCES
+const ionIconSchool = document.getElementById("ionIconSchool");
+
+const ionIconSad = document.getElementById("ionIconSad");
+
+const ionIconMusicalNotes = document.getElementById("ionIconMusicalNotes");
+
+const ionIconVideo = document.getElementById("ionIconVideo");
+
+const ionIconOthers = document.getElementById("ionIconOthers");
+
+// ****
+
+preferencesToggle.addEventListener("click", ()=>{
+  preferencesMenu_index.classList.toggle("active");
+  ionIconSad.style.transform="rotate(-80deg)";
+  ionIconMusicalNotes.style.transform="rotate(-100deg)";
+  ionIconVideo.style.transform="rotate(90deg)";
+  ionIconOthers.style.transform="rotate(80deg)";
+});
+
+// ***
+
+  // ANIMATION TOGGLE BTN
+  const preferencesToggleIonIcon = document.getElementById("preferencesToggleIonIcon");
+  const intervalToggle = setInterval(()=>{
+
+  preferencesToggleIonIcon.style.fontSize="2.7rem";
 
   setTimeout(() => {
-    preferencesContainer_index.style.display="flex";
-    containerNamePage_index.style.display="flex";
-    showPreferencesButton.style.opacity="1";
-    
-  }, "200");
+    preferencesToggleIonIcon.style.fontSize="3.5rem";
+  }, "600");
 
-  setTimeout(() => {
-    preferencesContainer_index.style.transform="translateY(0%)";
-    showPreferencesButton.style.display="flex";
-  }, "500");
+}, 700);
 
-  setTimeout(() => {
-    preferencesContainer_index.style.opacity="1";
-    containerNamePage_index.style.opacity="1";
-    
-  }, "400");
+// CLEAR INTERVAL ON CLICK 
+// clearInterval(nIntervId);
 
+// ***
 
-// *********
+const preferenceRadioBtn0 = document.getElementById("preferenceRadioBtn0");
+const preferenceRadioBtn1 = document.getElementById("preferenceRadioBtn1");
+const preferenceRadioBtn2 = document.getElementById("preferenceRadioBtn2");
+const preferenceRadioBtn3 = document.getElementById("preferenceRadioBtn3");
+const preferenceRadioBtn4 = document.getElementById("preferenceRadioBtn4");
+// const preferenceRadioBtn5 = document.getElementById("preferenceRadioBtn5");
+// const preferenceRadioBtn6 = document.getElementById("preferenceRadioBtn6");
+// const preferenceRadioBtn7 = document.getElementById("preferenceRadioBtn7");
+// const preferenceRadioBtn8 = document.getElementById("preferenceRadioBtn8");
 
-preference1.addEventListener("click",  ()=>{
-
-  namePreferenceTxt.innerText="¡FIESTA!";
-  
+// PREFERENCE BTNS ONCLICK EVENTS
+preferenceRadioBtn0.addEventListener("click", ()=>{
+  containerPreferenceBtnDescription_index.style.display="flex";
+  preferenceBtnDescriptionTxt.innerText="FORMACIÓN";
+  vamosBtn_index.style.display="block";
 });
 
-preference2.addEventListener("click",  ()=>{
-
-  namePreferenceTxt.innerText="ARTE";
-
+preferenceRadioBtn1.addEventListener("click", ()=>{
+  containerPreferenceBtnDescription_index.style.display="flex";
+  preferenceBtnDescriptionTxt.innerText="TEATRO";
+  vamosBtn_index.style.display="block";
 });
 
-preference3.addEventListener("click",  ()=>{
-
-  namePreferenceTxt.innerText="MÚSICA";
-
+preferenceRadioBtn2.addEventListener("click", ()=>{
+  containerPreferenceBtnDescription_index.style.display="flex";
+  preferenceBtnDescriptionTxt.innerText="MÚSICA";
+  vamosBtn_index.style.display="block";
 });
 
-preference4.addEventListener("click",  ()=>{
-
-  namePreferenceTxt.innerText="GASTRONOMÍA";
-
+preferenceRadioBtn3.addEventListener("click", ()=>{
+  containerPreferenceBtnDescription_index.style.display="flex";
+  preferenceBtnDescriptionTxt.innerText="CINE";
+  vamosBtn_index.style.display="block";
 });
 
-preference5.addEventListener("click",  ()=>{
-
-  namePreferenceTxt.innerText="AL AIRE LIBRE";
-
+preferenceRadioBtn4.addEventListener("click", ()=>{
+  containerPreferenceBtnDescription_index.style.display="flex";
+  preferenceBtnDescriptionTxt.innerText="OTROS";
+  vamosBtn_index.style.display="block";
 });
 
-preference6.addEventListener("click",  ()=>{
+// preferenceRadioBtn5.addEventListener("click", ()=>{
+//   containerPreferenceBtnDescription_index.style.display="flex";
+//   preferenceBtnDescriptionTxt.innerText="DEPORTE";
+//   vamosBtn_index.style.display="block";
+// });
 
-  namePreferenceTxt.innerText="DEPORTES";
+// preferenceRadioBtn6.addEventListener("click", ()=>{
+//   containerPreferenceBtnDescription_index.style.display="flex";
+//   preferenceBtnDescriptionTxt.innerText="ALTERNATIVO";
+//   vamosBtn_index.style.display="block";
+// });
 
-});
+// preferenceRadioBtn7.addEventListener("click", ()=>{
+//   containerPreferenceBtnDescription_index.style.display="flex";
+//   preferenceBtnDescriptionTxt.innerText="¡FIESTA!";
+//   vamosBtn_index.style.display="block";
+// });
 
-preference7.addEventListener("click",  ()=>{
+// preferenceRadioBtn8.addEventListener("click", ()=>{
+//   containerPreferenceBtnDescription_index.style.display="flex";
+//   preferenceBtnDescriptionTxt.innerText="AL AIRE LIBRE";
+//   vamosBtn_index.style.display="block";
+// });
 
-  namePreferenceTxt.innerText="ALTERNATIVO";
-
-});
-
-preference8.addEventListener("click",  ()=>{
-
-  namePreferenceTxt.innerText="RANDOM";
-  
-});
-
-// *********
-
-showPreferencesButton.addEventListener("click", ()=>{
-
-  setTimeout(() => {
-    preferencesContainer_index.style.display="none";
-    showPreferencesButton.style.opacity="0";
-  }, "400");
-
-  setTimeout(() => {
-    preferencesContainer_index.style.transform="translateY(30%)";
-  }, "200");
-
-  setTimeout(() => {
-    preferencesContainer_index.style.opacity="0";
-    containerNamePage_index.style.opacity="0";
-    containerNamePage_index.style.display="none";
-    showPreferencesButton.style.display="none";
-  }, "200");
-
-});
-
-
-// *********
-
-
-async function getData(){
-  
-    const url = new URL("https://api.euskadi.eus/culture/events/v1.0/events")
-    
-    url.searchParams.append("_elements",100);
-
-    const response = await fetch(url.toString());
-    
-    const data =  await response.json();
-
-    addEvents(data.items);
-
-}
-
-function addEvents(events){
-    
-    let typeArray = [];
-    // let name;
-
-    for(let i=0; i < events.length; i++){
-    
-        typeArray[i] =  events[i].typeEs;
-        
-    }
-
-    const ns = new Set(typeArray);
-    const uniqueTypeArray = [...ns];
-    console.log(uniqueTypeArray);
-
-    for(let j=0; j < uniqueTypeArray.length; j++){
-      const name = document.createElement("p");
-      name.innerText = uniqueTypeArray[j]; 
-      divEvents.appendChild(name);
-      
-    }
-    
-};
-
-
-
-async function getData(){
-    const url = new URL("https://api.euskadi.eus/culture/events/v1.0/events")
-    url.searchParams.append("_elements",200);
-    console.log(url.toString());
-    
-  
-    const response = await fetch(url.toString());
-    //console.log(response);
-    const data =  await response.json();
-    console.log(data);
-    addEvents(data.items)
-
-}
-
-function addEvents(events){
-    const divEvents = document.getElementById("eventos");
-    events.forEach((event,index) => {
-        const name = document.createElement("h1");
-        name.innerText=index + "- "+event.nameEs
-        const typeEs = document.createElement("h2");
-        typeEs.innerText=index + "- "+event.typeEs
-        
-        divEvents.appendChild(name);
-        divEvents.appendChild(typeEs);
-
-    });
-}
-
-getData();
+// ***
