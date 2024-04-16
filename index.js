1cb07f1f4d3d606dbe1b54f86e5a532dab14916f
Gandino1984 <andinogerman@gmail.com>
2024-04-16 12:15:07 +0200

    css changes

--- index.js
+++ index.js
@@ -10,23 +10,31 @@ const preferenceBtnDescriptionTxt = document.getElementById("preferenceBtnDescri
 
 const vamosBtn_index = document.getElementById("vamosBtn_index");
 
-const strFormacion = " - Formaciones - Formaciones - ";
-
-const strTeatro = " - Teatro - Teatro - Teatro ";
-
-const strMusica = " - Música - Música - Música ";
-
-const strCine = " - Cine - Cine - Cine -";
-
-const strOtros = " - Otros - Otros - Otros -";
-
-const strRandom = " - Random - Random - Random - "
 
 // ***
+// CARGAR TARJETAS DE MUESTRA ALEATORIAS EN LA PÁGINA INICIAL
+const systemDate = new Date();
+
+  console.log("systemDate" + systemDate);
+  console.log("Día: " + systemDate.getDate());
+  console.log("Año: " + systemDate.getFullYear());
+  console.log("Mes: " + systemDate.getMonth() + 1);
+
+
+// window.addEventListener("load", (event) => {
+  
+// });
+
+
+// *** PREFERENCES TOGGLE BTN
+let togglePressed = false;
 
 preferencesToggle.addEventListener("mouseover", ()=>{
   preferencesToggle.style.transform = "rotate(1turn)";
+
 });
+
+// ***
 
 preferencesToggle.addEventListener("mouseleave", ()=>{
   preferencesToggle.style.transform = "rotate(-1turn)";
@@ -35,19 +43,24 @@ preferencesToggle.addEventListener("mouseleave", ()=>{
 // ***
 
 const ionIconSchool = document.getElementById("ionIconSchool");
-
 const ionIconSad = document.getElementById("ionIconSad");
-
 const ionIconMusicalNotes = document.getElementById("ionIconMusicalNotes");
-
 const ionIconVideo = document.getElementById("ionIconVideo");
-
 const ionIconOthers = document.getElementById("ionIconOthers");
 
 // ****
 
 preferencesToggle.addEventListener("click", ()=>{
-  containerPreferencesMenu_index.style.backgroundColor="#8ecae67b";
+  if(togglePressed == false){
+    preferenceBtnDescriptionTxt.style.display="flex";
+    containerPreferencesMenu_index.style.backgroundColor="#8ecae67b";
+    togglePressed=true;
+  }else{
+    preferenceBtnDescriptionTxt.style.display="none";
+    containerPreferencesMenu_index.style.backgroundColor="#000";
+    togglePressed=false;
+  }
+
   preferencesMenu_index.classList.toggle("active");
   ionIconSad.style.transform="rotate(-80deg)";
   ionIconMusicalNotes.style.transform="rotate(-100deg)";
@@ -55,11 +68,11 @@ preferencesToggle.addEventListener("click", ()=>{
   ionIconOthers.style.transform="rotate(80deg)";
 });
 
-// ***
+  // ***
 
   // ANIMATION TOGGLE BTN
-  const preferencesToggleIonIcon = document.getElementById("preferencesToggleIonIcon");
-  const intervalToggle = setInterval(()=>{
+const preferencesToggleIonIcon = document.getElementById("preferencesToggleIonIcon");
+const intervalToggle = setInterval(()=>{
 
   preferencesToggleIonIcon.style.fontSize="2.7rem";
 
@@ -69,8 +82,6 @@ preferencesToggle.addEventListener("click", ()=>{
 
 }, 700);
 
-// CLEAR INTERVAL ON CLICK 
-// clearInterval(nIntervId);
 
 // ***
 
@@ -79,13 +90,19 @@ const preferenceRadioBtn1 = document.getElementById("preferenceRadioBtn1");
 const preferenceRadioBtn2 = document.getElementById("preferenceRadioBtn2");
 const preferenceRadioBtn3 = document.getElementById("preferenceRadioBtn3");
 const preferenceRadioBtn4 = document.getElementById("preferenceRadioBtn4");
+const preferenceRadioBtn5 = document.getElementById("preferenceRadioBtn5");
 
+const strFormacion = " - Formaciones - Formaciones - ";
+const strTeatro = " - Teatro - Teatro - Teatro ";
+const strMusica = " - Música - Música - Música ";
+const strCine = " - Cine - Cine - Cine -";
+const strOtros = " - Otros - Otros - Otros -";
+const strRandom = " - Random - Random - Random - "
 
 // PREFERENCE BTNS ONCLICK EVENTS
 preferenceRadioBtn0.addEventListener("click", ()=>{
   
   containerPreferenceBtnDescription_index.style.display="flex";
-  
   
   preferenceBtnDescriptionTxt.innerText="";
   
@@ -97,8 +114,8 @@ preferenceRadioBtn0.addEventListener("click", ()=>{
     
     span.style.transform=`rotate(${11 * i}deg)`;
   }
-
-  vamosBtn_index.style.display="block";
+  vamosBtn_index.classList.toggle("btnAnimation");
+  // vamosBtn_index.style.="block";
 
 });
 
@@ -188,5 +205,13 @@ preferenceRadioBtn5.addEventListener("click", ()=>{
 });
 
 
+// *** BOTON BUSCAR (vamosbTN)
+let vamosBtnWasPressed = false;
 
-// ***
+vamosBtn_index-addEventListener("click", ()=>{
+
+  // CLEAR INTERVAL TOGGLE BTN ON CLICK OF VAMOSBTN 
+  clearInterval(intervalToggle);
+
+});
+
